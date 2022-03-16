59b26f94c61a8b954a14f0dbec124929d155cda1
Doobu-L <60733417+Doobu-L@users.noreply.github.com>
2022-03-16 14:14:55 +0900

    Update 영어끝말잇기.java

--- src/main/java/easy/영어끝말잇기.java
+++ src/main/java/easy/영어끝말잇기.java
@@ -20,3 +20,22 @@ class Solution {
 		return answer;
 	}
 }
+
+--->>>>
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+		int[] answer = {0,0};
+		Set<String> wordSet=new HashSet<>();
+		wordSet.add(words[0]);
+		for(int i=1;i<words.length;i++){
+			if(!wordSet.add(words[i]) || !words[i].startsWith(words[i-1].substring(words[i-1].length()-1))){
+				answer[0]=i%n+1;
+				answer[1]=i/n+1;
+				break;
+			}
+		}
+		return answer;
+	}
+}
