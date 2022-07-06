79e01e291942bfdbb8cb35493de168cb0d62115c
Dyo <60733417+Doobu-L@users.noreply.github.com>
2022-07-06 18:49:34 +0900

    Update level2-1try.java

--- src/main/java/Programers/level2-1try.java
+++ src/main/java/Programers/level2-1try.java
@@ -47,7 +47,19 @@ class Solution
 }
 
 //정확성 34.8 , 효율성 0
+//다시 stream 없이 하니 통과했다..
+public int solution(int []A, int []B)
+  {
+    int answer = 0;
+    Arrays.sort(A);
+    Arrays.sort(B);
 
+    for(int i=0;i<A.length;i++){
+      answer+=A[i]*B[A.length-1-i];
+    }
+
+    return answer;
+  }
 
 
 /*
