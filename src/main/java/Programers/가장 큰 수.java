6826a398d44bd3f1ab4df11287d8ce603c5e059d
Dyo <60733417+Doobu-L@users.noreply.github.com>
2022-07-20 14:59:24 +0900

    Update 가장 큰 수.java

--- src/main/java/Programers/가장 큰 수.java
+++ src/main/java/Programers/가장 큰 수.java
@@ -48,3 +48,25 @@ class Solution {
 
   }
 }
+
+/*
+  생각을 잘못하고있었다. 위의 방식으로 해결하려 할 경우, 312 , 31 -> 312222.compareTo(311111) 이 된다
+  31312 > 31231 큼으로 31312(o) 지만 31231 이 나옴다.
+  그럼 방법은 숫자를 채워주는 format을 333312 , 333331 처럼 앞을 채울 것인지 뒤를 채울 것인지를 결정해주는 구문이 들어가야 한다.
+  이 방식으로 하면 코드도 길어지고 , 난해해 지기 때문에 다른 방법을 생각했다.
+  
+  앞+뒤 .compareTo 뒤+앞 이다.
+  31231.compareTo 31312
+  
+  내림차순으로 정렬할 것으로 뒤+앞.compareTo 앞+뒤
+  
+*/
+
+class Solution {
+
+  public String solution(int[] numbers) {
+    String answer = Arrays.stream(numbers).mapToObj(n->String.valueOf(n)).sorted((s, t1) -> t1.concat(s).compareTo(s.concat(t1))).collect(
+        Collectors.joining());
+    return answer.startsWith("0") ? "0":answer;
+  }
+}
